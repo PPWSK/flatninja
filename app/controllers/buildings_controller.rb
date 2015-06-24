c5d945770423f16a3c641a7a0af5f556209ea5a4
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-06-24 21:20:24 +0200

    added javascript and icons

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,24 +1,49 @@
 class BuildingsController < ApplicationController
   skip_before_action :authenticate_account!, only: [ :show, :index ]
 
+  before_action :find_user, only: [ :new, :create, :edit, :update ]
+
   def index
+
   end
 
   def show
+
   end
 
   def new
+    # raise @user.inspect
+    @building = Building.new
   end
 
   def create
+    @building = Building.create(building_params)
+    @building.user = @user
+      if @building.save
+        redirect_to buildings_path
+      else
+        render :new
+      end
   end
 
   def edit
+
   end
 
   def update
+
   end
 
-  def destroy
+  private
+
+  def building_params
+    params.require(:building).permit(:name, :building_type, :location,
+      :number_of_rooms, :number_of_roommates, :garden, :balcony, :bathroom,
+      :kitchen, :pets, :cleaning, :garage, :bike_storage)
   end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
 end
