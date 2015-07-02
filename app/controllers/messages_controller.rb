3b365c9c30cb81031125a60a845d846adf81e061
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-07-02 18:17:59 +0200

    fixed messages more

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
 
-  before_action :find_eval, only: [ :new, :create, :show, :index ]
+  # before_action :find_eval, only: [ :new, :create, :show, :index ]
   before_action :find_user, only: [ :new, :create, :show, :index ]
+
   def new
   end
 
@@ -12,7 +13,14 @@ class MessagesController < ApplicationController
   end
 
   def index
-    @messages = Message.where(recipient_id: current_account.user.id, read: [nil, false] )
+    @messages = Message.where("recipient_id = ? or sender_id = ?", @user.id, @user.id)
+    @messages.each do |msg|
+      if msg.recipient_id == @user.id
+        msg.read = true
+        msg.save!
+      end
+    end
+
   end
 
   def find_user
@@ -20,7 +28,7 @@ class MessagesController < ApplicationController
     @user = current_account.user
   end
 
-  def find_eval
-    @eval = Evaluation.find(params[:evaluation_id])
-  end
+  # def find_eval
+  #   @eval = Evaluation.find(params[:evaluation_id])
+  # end
 end
