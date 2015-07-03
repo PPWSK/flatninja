1e6106ec4b2fe981afe9ace20d94dbc66ad754c7
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-07-03 04:29:19 +0200

    designed entire messaging screen

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,17 +7,34 @@ class MessagesController < ApplicationController
   end
 
   def create
+
   end
 
   def show
   end
 
   def index
+
     @messages = Message.where("recipient_id = ? or sender_id = ?", @user.id, @user.id)
     @messages.each do |msg|
       if msg.recipient_id == @user.id
         msg.read = true
         msg.save!
+      end
+    end
+
+    @msg_by_eval = @messages.group_by(&:evaluation_id)
+
+    if params[:extra] != nil
+      @right_eval = Evaluation.find(params[:extra])
+      @right_messages = @right_eval.messages
+    else
+      if !@msg_by_eval.empty? && !@msg_by_eval.first[0].nil?
+        @right_eval = Evaluation.find(@msg_by_eval.first[0])
+        @right_messages = @right_eval.messages
+      else
+        flash[:alert] = "No messages! If you think something went wrong, please contact support service."
+        redirect_to root_path
       end
     end
 
